00db748f8d1f77fd7f8739be4c9c242d3b6be1dc
TuanAnhNguyen14111998 <tuananh14111998@gmail.com>
2019-10-19 16:40:13 +0700

    concat services

---
+++ drugs_side_effect/common_service/constants.py
@@ -0,0 +1,15 @@
+TRAINED_MODEL = "weights/craft_mlt_25k.pth"
+TEXT_THRESHOLD = 0.7
+LOW_TEXT = 0.4
+LINK_THRESHOLD = 0.4
+CUDA = True
+CANVAS_SIZE = 1280
+MAG_RATIO = 1.5
+POLY = False
+SHOW_TIME = False
+TEST_FORDEL = "/data/"
+REFINE = False
+REFINER_MODEL = "weights/craft_refiner_CTW1500.pth"
+
+MODEL_PATH_CRNN = "./data/crnn.pth"
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz"
